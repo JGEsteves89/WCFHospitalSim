fe9127c174f1180aa31facb9bf56a30653fd8f14
rffreitas <ricardoffreitas@altran.com>
2018-05-29 13:26:49 +0100

    Unit Tests

---
+++ sourcecode/SharedLibraryUnitTest/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SharedLibray;
+
+namespace SharedLibraryUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void NewPatientConstructorTest()
+        {
+            Patient newPatient = new Patient("Name", 2);
+
+            Assert.AreEqual(newPatient.name, "Name");
+            Assert.AreEqual(newPatient.age, 2);
+        }
+    }
+}
---
+++ sourcecode/WcfServiceAUnitTest/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WcfServiceAUnitTest.ServiceReferenceA;
+
+namespace WcfServiceAUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestConnectionOk()
+        {
+            ServiceAClient newClient = new ServiceAClient();
+
+            Assert.AreEqual(newClient.ConnectionOK(), true);
+        }
+
+        [TestMethod]
+        public void ListPatientsFromBNotNull()
+        {
+            ServiceAClient newClient = new ServiceAClient();
+
+            Assert.AreEqual(newClient.ConnectionOK(), true);
+            Assert.IsNotNull(newClient.getPatientsFromB());
+        }
+    }
+}
